4bdd59a2edd73268658d002fb253ba8f99d11254
Armand Zangue <armand.zangue@gmail.com>
2015-12-27 20:00:11 +0100

    changed all '\' to DS constant

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -15,7 +15,7 @@ use Noodlehaus\Config;
 ini_set('display_errors', 'On');
 
 // Require all PHP dependencies using the composer autoloader
-require ROOT . DS . 'vendor/autoload.php';
+require ROOT . DS . 'vendor' . DS . 'autoload.php';
 
 require ROOT . DS . CORE_DIR . DS . 'Router.php';
 require ROOT . DS . CORE_DIR . DS . 'Model' . DS . 'Model.php';
@@ -24,10 +24,10 @@ require ROOT . DS . CORE_DIR . DS . 'Controller' . DS . 'Controller.php';
 require ROOT . DS . CORE_DIR . DS . 'Controller' . DS . 'ErrorController.php';
 require ROOT . DS . CORE_DIR . DS . 'View' . DS . 'View.php';
 
-require APP_DIR . DS . 'Controller/AppController.php';
-require APP_DIR . DS . 'Controller/HelloController.php';
-require APP_DIR . DS . 'Model/AppModel.php';
-require APP_DIR . DS . 'Model/HelloModel.php';
+require APP_DIR . DS . 'Controller' . DS . 'AppController.php';
+require APP_DIR . DS . 'Controller' . DS . 'HelloController.php';
+require APP_DIR . DS . 'Model' . DS . 'AppModel.php';
+require APP_DIR . DS . 'Model' . DS . 'HelloModel.php';
 
 // Instantiate a new Slim Application
 $app = new Slim([
@@ -40,6 +40,6 @@ $app = new Slim([
 
 // Load configuration
 $app->configureMode($app->config('mode'), function () use ($app) {
-	$app->config = Config::load(APP_DIR . DS . "Config/{$app->mode}.php");
+	$app->config = Config::load(APP_DIR . DS . "Config" . DS . "{$app->mode}.php");
 });
 
